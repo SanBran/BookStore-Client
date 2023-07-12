40f72060339cc45c75223d8e1cec4957c79931bd
SanBran <luminoforos@gmail.com>
2023-07-12 00:15:52 -0600

    BookStore

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Book Store</h1> 
+    </>
+  )
+}
+
+export default App
---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+let initialState = {};
+
+const reducer = (state = initialState, action) => {
+  // switch (action.type) {
+  //   case GET:
+  //     return {
+  //       ...state,
+  //     };
+  //   default:
+  //     return { ...state };
+  // }
+};
+
+export default reducer;
