53e7bcfe9d2e19fc1ad39ba293e43e2ed384401d
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-07-18 17:27:24 -0300

    nueva view Access y login y singup pasan a ser componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
-import Login from "./Views/Login/Login";
+import Login from "./Components/Login/Login";
 import Home from "./Views/Home/Home";
 import BooksDetail from "./Views/BooksDetail/BooksDetail";
 import "./App.css";
-import SignUp from "./Views/Signup/Signup";
+import SignUp from "./Components/Signup/Signup";
+import Access from "./Views/Access/Access";
 
 function App() {
   const location = useLocation();
   return (
     <>
-      <h1>Book Store</h1>
-      {location.pathname !== "/" ? <Dashboard /> : <Login />}
+      {/* <h1>Book Store</h1> */}
+      {location.pathname !== "/" ? <Dashboard /> : <Access />}
       <Routes>
         <Route path="/home" element={<Home />} />
 
