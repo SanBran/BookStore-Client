539f3ac5fceb0f9f37244486e03ccafbd05e87f8
OmarnLujan <127269393+OmarnLujan@users.noreply.github.com>
2023-07-25 19:59:49 -0300

    estado cart y favorito

--- src/Components/CardBook/Book.jsx
+++ src/Components/CardBook/Book.jsx
@@ -1,34 +1,110 @@
 //import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Book.module.css'
+import { Link } from "react-router-dom";
+import styles from "./Book.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import {
+  addCart,
+  addFavorite,
+  getUserById,
+  removeCart,
+} from "../../redux/actions/actions";
 
 const Book = ({ books }) => {
-    const { id, image, title, author, price } = books;
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.access);
+  const userDat = useSelector((state) => state.userDetail);
 
-    const genericCover = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhRKhJb1aLmjwGX_ox0TA6eTxCv_5g3Nlr6w&usqp=CAU"
+  useEffect(() => {
+    dispatch(getUserById(user.ref));
+  }, [dispatch, user.ref]);
 
-    return (
-        <div className={styles.container}>
+  const { id, image, title, author, price } = books;
 
-            <Link className={styles.image} to={`/detail/${id}`}>
-                <img
-                    className={styles.imageSize}
-                    src={image !== 'Image not Available' ? image : genericCover}
-                    alt={`${title} from ${author}`}
-                />
-            </Link>
+  const [userData, setUserData] = useState(userDat);
 
-            <div className={styles.textContainer}>
-                <div className={styles.title}>
-                    {title}
-                </div>
-                <div className={styles.author}>
-                    {author}
-                </div>
-                {price && price ? <div className={styles.price}>${price}</div> : <div className={styles.price}>Free</div>}
-            </div>
-        </div>
-    )
-}
+  const [isFav, setIsFav] = useState(false);
+  const [cart, setCart] = useState(false);
+
+  const genericCover =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhRKhJb1aLmjwGX_ox0TA6eTxCv_5g3Nlr6w&usqp=CAU";
+
+  const handleWishlist = async () => {
+    let list;
+    if (isFav) {
+      setIsFav(false);
+      list = userData.listWish.filter((bookF) => bookF.id !== id);
+    } else {
+      setIsFav(true);
+      userData.listWish
+        ? (list = [...userData.listWish, books])
+        : (list = [books]);
+    }
+    sendData(list);
+  };
+  const sendData = (list)=>{
+    console.log(userData);
+    userData.listWish=list;
+    dispatch(addFavorite(userData));
+
+} 
+  const handleCart = () => {
+    if (cart) {
+      setCart(false);
+      dispatch(removeCart(books.id));
+    } else {
+      setCart(true);
+      dispatch(addCart(books));
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      {user.state ? (
+        isFav ? (
+          <button onClick={handleWishlist} className={styles.boton}>
+            🧡
+          </button>
+        ) : (
+          <button onClick={handleWishlist} className={styles.boton}>
+            🤍
+          </button>
+        )
+      ) : (
+        <></>
+      )}
+      {user.state ? (
+        cart ? (
+          <button onClick={handleCart} className={styles.boton2}>
+            🛒
+          </button>
+        ) : (
+          <button onClick={handleCart} className={styles.boton2}>
+            🛍️
+          </button>
+        )
+      ) : (
+        <></>
+      )}
+      <Link className={styles.image} to={`/detail/${id}`}>
+        <img
+          className={styles.imageSize}
+          src={image !== "Image not Available" ? image : genericCover}
+          alt={`${title} from ${author}`}
+        />
+      </Link>
+
+      <div className={styles.textContainer}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.author}>{author}</div>
+        {price && price ? (
+          <div className={styles.price}>${price}</div>
+        ) : (
+          <div className={styles.price}>Free</div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Book;
