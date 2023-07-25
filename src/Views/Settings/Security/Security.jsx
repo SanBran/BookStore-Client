cdccda7141a2825baf1f0eb00bcd89413a4e557f
OmarnLujan <127269393+OmarnLujan@users.noreply.github.com>
2023-07-25 16:33:30 -0300

    Setting/Mydata

---
+++ src/Views/Settings/Security/Security.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Security({ onBack }) {
+  return (
+    <div>
+    
+      <h2>Security</h2>
+      <button onClick={onBack}>Back</button>
+      <br />
+    </div>
+  );
+}
+
+export default Security;
