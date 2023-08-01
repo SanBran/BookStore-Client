f9911a40bdf9da603e1c0702cf31d82c5f8f3132
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-08-01 16:32:12 -0300

    estilos en Settings

--- src/Views/Settings/Settings.jsx
+++ src/Views/Settings/Settings.jsx
@@ -5,11 +5,15 @@ import { useSelector } from "react-redux";
 import MyData from "./MyData/MyData";
 import Security from "./Security/Security";
 
+//----icons
+import profile_icon from '../../assets/icons/profile.svg';
+import edit_icon from '../../assets/icons/edit_icon.svg';
+
 
 function Settings() {
   const user = useSelector((state) => state.userDetail);
 
-  const [currentView, setCurrentView] = useState(""); // Estado para controlar qué componente mostrar
+  const [currentView, setCurrentView] = useState("data"); // Estado para controlar qué componente mostrar
   const handleBack = () => {
     setCurrentView(); // Ocultamos el menú para volver al componente anterior
   };
@@ -17,19 +21,38 @@ function Settings() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.containerData}>
-        <img src={user.photoUser} width="70" height="70" />
-        <h2 style={{ color: "BLACK" }}>{user.name}</h2>
-      </div>
-      <div>
-        {currentView === "data" && <MyData user={user}onBack={handleBack} />}
-        {currentView === "security" && <Security user={user} onBack={handleBack} />}
-        {currentView !== "data" && currentView !== "security" && (
-          <button onClick={() => setCurrentView("data")}>My Data</button>
-        )}
-        {currentView !== "data" && currentView !== "security" && (
-          <button onClick={() => setCurrentView("security")}>Security</button>
-        )}
+      <div className={styles.containerPadding}>
+        <div className={styles.containerData}>
+          <h2 className={styles.title}>Settings</h2>
+          <div className={styles.containerImg}>
+            <img className={styles.userImg} src={user.photoUser ? user.photoUser : profile_icon} width="70" height="70" />
+            <img className={styles.editImg} src={edit_icon} alt="edit" />
+          </div>
+          <h2 className={styles.name}>{user.name}</h2>
+        </div>
+
+        <div className={styles.formsContainer}>
+          <div className={styles.switchForms}>
+            <div 
+            className={currentView === "data" ? styles.switchFocus : styles.switch}
+            onClick={() => setCurrentView("data")}
+            >
+                <h1 className={styles.switchTitle}>My Data</h1>
+            </div>
+            <div 
+            className={currentView === "security" ? styles.switchFocus : styles.switch}
+            onClick={() => setCurrentView("security")}
+            >
+                <h1 className={styles.switchTitle}>Security</h1>
+            </div>
+          </div>
+
+          <div className={styles.form}>
+            {currentView === "data" && <MyData user={user}onBack={handleBack} />}
+            {currentView === "security" && <Security user={user} onBack={handleBack} />}
+          </div>
+
+        </div>
       </div>
     </div>
   );
