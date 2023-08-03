7a37b2742f59f2e4913c0d8865e7d2a21e17f7a8
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-08-03 16:55:00 -0300

    se agregro del icon del carrito al home

--- src/Components/Navbar/NavbarNoLogo.jsx
+++ src/Components/Navbar/NavbarNoLogo.jsx
@@ -3,6 +3,7 @@ import styles from "./Navbar.module.css";
 import SearchBar from "../SearchBar/SearchBar";
 import { overlayProfile } from "../../redux/actions/actions";
 import { useDispatch, useSelector } from "react-redux";
+import cart_icon from '../../assets/icons/cart_icon.svg';
 import profileLogo from "../../sources/profile-user.png";
 import Cart from "../../Views/Cart.jsx/Cart";
 import { useState } from "react";
@@ -13,6 +14,7 @@ const Navbar = () => {
   const dispatch = useDispatch();
   const showOverlayPerfile = useSelector((state) => state.overlayProfile);
   const access = useSelector((state) => state.access.state);
+  const cart = useSelector((state) => state.cart);
 
   const handleOverlayToggle = () => {
     if (access) {
@@ -44,14 +46,15 @@ const Navbar = () => {
         <SearchBar />
       </div>
       {access ? (
-        <div className={styles.crearEsteEstilo}>
-          <div>
-            <button className={styles.noquieroesteboton} onClick={openModal}></button>
-            <Cart
-              isOpen={modalIsOpen}
-              onRequestClose={closeModal}
-            />
-          </div>
+        <div className={styles.cartContainer}>
+          <img className={styles.cartIcon} src={cart_icon} alt="🛒" onClick={openModal} />
+          <Cart
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+          />
+          {(cart && cart.length !== 0)
+            ? (<div className={styles.cartCounter}>{cart.length}</div>)
+            : (<></>)}
         </div>
       ) : (
         <></>
